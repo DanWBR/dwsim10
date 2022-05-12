d7e0d7f31b7d013475e77d354acc7c3704e6753d
DanWBR <danielwag@gmail.com>
2022-05-12 11:12:55 -0400

    removed GPL license text

--- DWSIMCore.Foundation/Thermodynamics/PropertyPackages/NISTMFAC.vb
+++ DWSIMCore.Foundation/Thermodynamics/PropertyPackages/NISTMFAC.vb
@@ -4,21 +4,7 @@
 '
 '    Based on the paper entitled "New modified UNIFAC parameters using critically 
 '    evaluated phase equilibrium data", http://dx.doi.org/10.1016/j.fluid.2014.12.042
-'
-'    This file is part of DWSIM.
-'
-'    DWSIM is free software: you can redistribute it and/or modify
-'    it under the terms of the GNU General Public License as published by
-'    the Free Software Foundation, either version 3 of the License, or
-'    (at your option) any later version.
-'
-'    DWSIM is distributed in the hope that it will be useful,
-'    but WITHOUT ANY WARRANTY; without even the implied warranty of
-'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-'    GNU General Public License for more details.
-'
-'    You should have received a copy of the GNU General Public License
-'    along with DWSIM.  If not, see <http://www.gnu.org/licenses/>.
+
 
 Imports DWSIMCore.Foundation.PropertyPackages
 Imports System.Math
