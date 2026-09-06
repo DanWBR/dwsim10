91930ed8759921bb0102a2043bdc85de0df9b453
Daniel Wagner Oliveira de Medeiros <danielwag@gmail.com>
2026-09-06 20:27:35 -0300

    feat: standalone binary free-radical copolymer CSTR solver (terminal model + moments)
    - CopolymerCSTR.Solve: isothermal steady state for two monomers by the terminal model; couples the monomer balances through the radical composition, gives the instantaneous copolymer composition (Mayo-Lewis) and the molar-mass averages from the method of moments with pseudo-kinetic constants and the average repeat-unit mass
    - CopolymerKinetics: reactivity ratios rA/rB plus Arrhenius homo-propagation, initiation and termination, with a styrene/MMA preset; MayoLewis helper
    - tests: ideal copolymerization tracks the feed, an azeotropic system holds F_A = 0.5, the composition equals Mayo-Lewis, the combination PDI limit (1.5), and a physical styrene/MMA benchmark

---
+++ engine/DWSIM.Thermodynamics/Polymers/CopolymerCSTR.vb
@@ -0,0 +1,212 @@
+'    Free-Radical Copolymerization CSTR - standalone steady-state solver (terminal model + moments)
+'    Copyright 2026 Daniel Wagner O. de Medeiros
+'
+'    This file is part of DWSIM.
+'
+'    DWSIM is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    DWSIM is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with DWSIM.  If not, see <http://www.gnu.org/licenses/>.
+
+Namespace Polymers
+
+    ''' <summary>
+    ''' Terminal-model kinetics for a binary free-radical copolymerization (monomers A and B). Propagation is
+    ''' described by the two homo-propagation constants and the two reactivity ratios rA = kpAA/kpAB and
+    ''' rB = kpBB/kpBA (so the cross constants are kpAB = kpAA/rA and kpBA = kpBB/rB). Each rate constant is
+    ''' Arrhenius, k = A*exp(-E/(R*T)); the reactivity ratios are taken temperature-independent. Termination is
+    ''' an average over the radical types.
+    ''' </summary>
+    Public Class CopolymerKinetics
+
+        Public Ad As Double, Ed As Double            ' initiator decomposition (1/s)
+        Public Efficiency As Double = 0.6
+        Public ApAA As Double, EpAA As Double        ' homo-propagation of A (L/mol/s)
+        Public ApBB As Double, EpBB As Double        ' homo-propagation of B (L/mol/s)
+        Public ReactivityA As Double = 1.0           ' rA = kpAA / kpAB
+        Public ReactivityB As Double = 1.0           ' rB = kpBB / kpBA
+        Public Atc As Double, Etc As Double          ' termination by combination (average, L/mol/s)
+        Public Atd As Double, Etd As Double          ' termination by disproportionation (average, L/mol/s)
+        Public MonomerAMW As Double                  ' molar mass of monomer A (g/mol)
+        Public MonomerBMW As Double                  ' molar mass of monomer B (g/mol)
+
+        Public Shared Function Arrhenius(A As Double, E As Double, T As Double) As Double
+            If A <= 0.0 Then Return 0.0
+            Return A * Math.Exp(-E / (8.314 * T))
+        End Function
+
+        ''' <summary>
+        ''' AIBN-initiated styrene (A) / methyl methacrylate (B) near 60 C, a classic copolymer pair:
+        ''' reactivity ratios rA = 0.52, rB = 0.46; the IUPAC styrene and MMA propagation constants;
+        ''' termination essentially by combination. Monomer molar masses 104.15 and 100.12 g/mol.
+        ''' </summary>
+        Public Shared Function StyreneMMA() As CopolymerKinetics
+            Return New CopolymerKinetics With {
+                .Ad = 1.58E+15, .Ed = 128000.0, .Efficiency = 0.6,
+                .ApAA = 4.266E+7, .EpAA = 32510.0,
+                .ApBB = 2.673E+6, .EpBB = 22360.0,
+                .ReactivityA = 0.52, .ReactivityB = 0.46,
+                .Atc = 1.255E+9, .Etc = 8000.0, .Atd = 0.0, .Etd = 0.0,
+                .MonomerAMW = 104.15, .MonomerBMW = 100.12}
+        End Function
+
+    End Class
+
+    ''' <summary>Steady-state result of a copolymer CSTR solve. Molar masses g/mol, concentrations mol/L.</summary>
+    Public Class CopolymerCSTRResult
+        Public Converged As Boolean
+        Public ConversionA As Double            ' 1 - [A]/[A]in
+        Public ConversionB As Double
+        Public OverallConversion As Double      ' total monomer converted / total fed
+        Public CopolymerCompositionA As Double  ' instantaneous mole fraction of A in the copolymer (Mayo-Lewis)
+        Public Mn As Double
+        Public Mw As Double
+        Public PDI As Double
+        Public Rp As Double                     ' total rate of polymerization (mol/L/s)
+        Public MonomerAConc As Double           ' outlet [A]
+        Public MonomerBConc As Double           ' outlet [B]
+        Public RadicalConc As Double            ' mu0
+        Public Iterations As Integer
+    End Class
+
+    ''' <summary>
+    ''' Standalone steady-state solver for a homogeneous, isothermal, binary free-radical copolymerization in a
+    ''' perfectly mixed reactor. The two monomer balances are coupled through the radical composition and are
+    ''' solved by successive substitution; the instantaneous copolymer composition follows the Mayo-Lewis
+    ''' equation (recovered here from the monomer consumption rates), and the molar-mass averages come from the
+    ''' method of moments with pseudo-kinetic propagation and termination constants and the average repeat-unit
+    ''' mass. Transfer is neglected in this version. Concentrations mol/L, residence time seconds, temperature K.
+    ''' </summary>
+    Public Class CopolymerCSTR
+
+        Public Shared Function Solve(kin As CopolymerKinetics, T As Double, ResidenceTime As Double,
+                                     MonomerAFeed As Double, MonomerBFeed As Double, InitiatorFeed As Double) As CopolymerCSTRResult
+
+            Dim res As New CopolymerCSTRResult()
+            Dim theta = ResidenceTime
+
+            Dim kd = CopolymerKinetics.Arrhenius(kin.Ad, kin.Ed, T)
+            Dim kpAA = CopolymerKinetics.Arrhenius(kin.ApAA, kin.EpAA, T)
+            Dim kpBB = CopolymerKinetics.Arrhenius(kin.ApBB, kin.EpBB, T)
+            Dim ktc = CopolymerKinetics.Arrhenius(kin.Atc, kin.Etc, T)
+            Dim ktd = CopolymerKinetics.Arrhenius(kin.Atd, kin.Etd, T)
+            Dim kt = ktc + ktd
+            Dim f = kin.Efficiency
+            Dim rA = kin.ReactivityA, rB = kin.ReactivityB
+            Dim kpAB = If(rA > 0.0, kpAA / rA, kpAA * 1.0E+6) ' cross constants from the reactivity ratios
+            Dim kpBA = If(rB > 0.0, kpBB / rB, kpBB * 1.0E+6)
+
+            ' Initiator (first-order) and the quasi-steady-state total radical concentration.
+            Dim I = InitiatorFeed / (1.0 + kd * theta)
+            Dim mu0 As Double = 0.0
+            If kt > 0.0 AndAlso I > 0.0 AndAlso kd > 0.0 Then mu0 = Math.Sqrt(f * kd * I / kt)
+            res.RadicalConc = mu0
+            res.MonomerAConc = MonomerAFeed
+            res.MonomerBConc = MonomerBFeed
+
+            If mu0 <= 0.0 Then
+                res.Converged = True
+                res.PDI = 1.0
+                Return res
+            End If
+
+            ' Couple the two monomer balances through the radical composition (successive substitution).
+            Dim A = MonomerAFeed, B = MonomerBFeed
+            Dim phiA As Double = 0.5, cA As Double, cB As Double
+            Dim it As Integer
+            For it = 1 To 200
+                Dim denom = kpBA * A + kpAB * B
+                phiA = If(denom > 0.0, kpBA * A / denom, 0.5)           ' fraction of A-ended radicals
+                Dim phiB = 1.0 - phiA
+                cA = mu0 * (kpAA * phiA + kpBA * phiB)                  ' pseudo first-order consumption of A
+                cB = mu0 * (kpBB * phiB + kpAB * phiA)
+                Dim Anew = MonomerAFeed / (1.0 + theta * cA)
+                Dim Bnew = MonomerBFeed / (1.0 + theta * cB)
+                Dim change = Math.Abs(Anew - A) + Math.Abs(Bnew - B)
+                A = 0.5 * Anew + 0.5 * A
+                B = 0.5 * Bnew + 0.5 * B
+                If change < 1.0E-12 * (MonomerAFeed + MonomerBFeed + 1.0E-30) Then Exit For
+            Next
+            res.Iterations = it
+
+            ' Recompute the rates at the converged composition.
+            Dim d2 = kpBA * A + kpAB * B
+            phiA = If(d2 > 0.0, kpBA * A / d2, 0.5)
+            cA = mu0 * (kpAA * phiA + kpBA * (1.0 - phiA))
+            cB = mu0 * (kpBB * (1.0 - phiA) + kpAB * phiA)
+
+            Dim consumeA = A * cA   ' mol/L/s of A incorporated
+            Dim consumeB = B * cB
+            Dim Rp = consumeA + consumeB
+            res.Rp = Rp
+
+            res.MonomerAConc = A
+            res.MonomerBConc = B
+            res.ConversionA = If(MonomerAFeed > 0.0, 1.0 - A / MonomerAFeed, 0.0)
+            res.ConversionB = If(MonomerBFeed > 0.0, 1.0 - B / MonomerBFeed, 0.0)
+            Dim fedTotal = MonomerAFeed + MonomerBFeed
+            res.OverallConversion = If(fedTotal > 0.0, ((MonomerAFeed - A) + (MonomerBFeed - B)) / fedTotal, 0.0)
+
+            ' Instantaneous copolymer composition (equals the Mayo-Lewis equation).
+            res.CopolymerCompositionA = If(Rp > 0.0, consumeA / Rp, 0.0)
+
+            ' Molar-mass averages by the method of moments with pseudo-kinetic constants and the average
+            ' repeat-unit mass. Transfer is neglected, so the propagation probability uses termination only.
+            Dim M = A + B
+            Dim kpBar = If(M > 0.0 AndAlso mu0 > 0.0, Rp / (mu0 * M), 0.0)
+            Dim Mbar = res.CopolymerCompositionA * kin.MonomerAMW + (1.0 - res.CopolymerCompositionA) * kin.MonomerBMW
+
+            Dim stopRate = kt * mu0
+            If M <= 0.0 OrElse stopRate <= 0.0 OrElse kpBar <= 0.0 Then
+                res.Converged = False
+                Return res
+            End If
+            Dim alpha = kpBar * M / (kpBar * M + stopRate)
+            Dim oneMinusAlpha = 1.0 - alpha
+            If oneMinusAlpha <= 0.0 Then
+                res.Converged = False
+                Return res
+            End If
+
+            Dim mu1 = mu0 / oneMinusAlpha
+            Dim mu2 = mu0 * (1.0 + alpha) / (oneMinusAlpha * oneMinusAlpha)
+            Dim G0 = (ktd + 0.5 * ktc) * mu0 * mu0
+            Dim G1 = kt * mu0 * mu1
+            Dim G2 = kt * mu0 * mu2 + ktc * mu1 * mu1
+            Dim L0 = theta * G0, L1 = theta * G1, L2 = theta * G2
+
+            If L0 > 0.0 AndAlso L1 > 0.0 Then
+                res.Mn = Mbar * L1 / L0
+                res.Mw = Mbar * L2 / L1
+                res.PDI = L0 * L2 / (L1 * L1)
+                res.Converged = True
+            Else
+                res.Converged = False
+            End If
+
+            Return res
+
+        End Function
+
+        ''' <summary>
+        ''' The instantaneous copolymer composition (mole fraction of A) from the Mayo-Lewis equation, for a
+        ''' given monomer mole fraction of A and the two reactivity ratios. Exposed for validation and plots.
+        ''' </summary>
+        Public Shared Function MayoLewis(fA As Double, rA As Double, rB As Double) As Double
+            Dim fB = 1.0 - fA
+            Dim num = rA * fA * fA + fA * fB
+            Dim den = rA * fA * fA + 2.0 * fA * fB + rB * fB * fB
+            Return If(den > 0.0, num / den, 0.0)
+        End Function
+
+    End Class
+
+End Namespace
---
+++ tests/DWSIM.Engine.SmokeTests/CopolymerCSTRTests.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+using DWSIM.Thermodynamics.Polymers;
+
+namespace DWSIM.Engine.SmokeTests
+{
+    /// <summary>
+    /// The standalone binary free-radical copolymer CSTR solver (terminal model + method of moments). Validated
+    /// against results that do not depend on the numerical details: the instantaneous copolymer composition
+    /// equals the Mayo-Lewis equation (ideal copolymerization tracks the feed, an azeotropic system holds its
+    /// composition), the polydispersity reaches its combination limit, and a styrene/MMA benchmark gives a
+    /// physically sensible composition and molar mass.
+    /// </summary>
+    [TestFixture]
+    public class CopolymerCSTRTests
+    {
+        [Test]
+        public void IdealCopolymerizationTracksTheMonomerComposition()
+        {
+            // rA = rB = 1 (ideal): the copolymer composition equals the monomer composition, F_A = f_A.
+            var k = CopolymerKinetics.StyreneMMA();
+            k.ReactivityA = 1.0; k.ReactivityB = 1.0;
+            var r = CopolymerCSTR.Solve(k, T: 333.15, ResidenceTime: 3600.0, MonomerAFeed: 4.0, MonomerBFeed: 4.0, InitiatorFeed: 0.02);
+            Assert.That(r.Converged, Is.True);
+            double fA = r.MonomerAConc / (r.MonomerAConc + r.MonomerBConc);
+            TestContext.WriteLine($"ideal: f_A={fA:F4} F_A={r.CopolymerCompositionA:F4}");
+            Assert.That(r.CopolymerCompositionA, Is.EqualTo(fA).Within(1e-6), "an ideal copolymer follows the monomer composition");
+        }
+
+        [Test]
+        public void AzeotropicCopolymerizationHoldsItsComposition()
+        {
+            // Symmetric reactivity ratios (rA = rB = 0.5) and equal propagation, equal feed: the azeotrope is at
+            // f_A = 0.5, so the copolymer comes out at F_A = 0.5.
+            var k = CopolymerKinetics.StyreneMMA();
+            k.ReactivityA = 0.5; k.ReactivityB = 0.5;
+            k.ApBB = k.ApAA; k.EpBB = k.EpAA;              // make B propagate like A (symmetric)
+            var r = CopolymerCSTR.Solve(k, T: 333.15, ResidenceTime: 3600.0, MonomerAFeed: 4.0, MonomerBFeed: 4.0, InitiatorFeed: 0.02);
+            Assert.That(r.Converged, Is.True);
+            TestContext.WriteLine($"azeotrope: F_A={r.CopolymerCompositionA:F4}");
+            Assert.That(r.CopolymerCompositionA, Is.EqualTo(0.5).Within(0.005), "an azeotropic system copolymerizes at F_A = 0.5");
+        }
+
+        [Test]
+        public void CompositionEqualsTheMayoLewisEquation()
+        {
+            var k = CopolymerKinetics.StyreneMMA();
+            var r = CopolymerCSTR.Solve(k, T: 333.15, ResidenceTime: 3600.0, MonomerAFeed: 5.0, MonomerBFeed: 3.0, InitiatorFeed: 0.02);
+            Assert.That(r.Converged, Is.True);
+            double fA = r.MonomerAConc / (r.MonomerAConc + r.MonomerBConc);
+            double ml = CopolymerCSTR.MayoLewis(fA, k.ReactivityA, k.ReactivityB);
+            TestContext.WriteLine($"mayo-lewis: f_A={fA:F4} F_A={r.CopolymerCompositionA:F4} ML={ml:F4}");
+            Assert.That(r.CopolymerCompositionA, Is.EqualTo(ml).Within(1e-6), "the solved composition must equal Mayo-Lewis");
+        }
+
+        [Test]
+        public void PureCombinationApproachesPDI_1_5()
+        {
+            var k = CopolymerKinetics.StyreneMMA();     // termination all by combination (Atd = 0)
+            var r = CopolymerCSTR.Solve(k, T: 333.15, ResidenceTime: 3600.0, MonomerAFeed: 4.0, MonomerBFeed: 4.0, InitiatorFeed: 1.0e-5);
+            Assert.That(r.Converged, Is.True);
+            TestContext.WriteLine($"combination: X={r.OverallConversion:F4} Mn={r.Mn:F0} PDI={r.PDI:F4}");
+            Assert.That(r.PDI, Is.EqualTo(1.5).Within(0.02), "combination termination gives a polydispersity of 3/2 for long chains");
+        }
+
+        [Test]
+        public void StyreneMMABenchmarkIsPhysical()
+        {
+            var k = CopolymerKinetics.StyreneMMA();
+            var r = CopolymerCSTR.Solve(k, T: 333.15, ResidenceTime: 3600.0, MonomerAFeed: 4.0, MonomerBFeed: 4.0, InitiatorFeed: 0.02);
+            TestContext.WriteLine($"styrene/MMA: X={r.OverallConversion:F4} F_A(styrene)={r.CopolymerCompositionA:F4} " +
+                                  $"Mn={r.Mn:F0} Mw={r.Mw:F0} PDI={r.PDI:F4}");
+            Assert.Multiple(() =>
+            {
+                Assert.That(r.Converged, Is.True);
+                Assert.That(r.OverallConversion, Is.InRange(0.01, 0.20), "a few percent conversion in one hour");
+                // rA, rB both < 1: an alternating tendency keeps the copolymer near equimolar for a 50/50 feed.
+                Assert.That(r.CopolymerCompositionA, Is.InRange(0.4, 0.6), "styrene fraction near equimolar for a 50/50 feed");
+                Assert.That(r.Mn, Is.InRange(1.0e4, 5.0e5), "number-average molar mass of order 1e5");
+                Assert.That(r.Mw, Is.GreaterThan(r.Mn), "weight-average exceeds number-average");
+            });
+        }
+    }
+}
